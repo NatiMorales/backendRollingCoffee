5e9d11aeb4027d841fc9a23b85e802f9367dc40a
Natalia Morales <natii.vmorales98@gmail.com>
2024-03-19 19:34:57 -0300

    agregamos una ruta en productos.routes y su respectivo controlador

---
+++ src/routes/productos.routes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { listaProductos } from "../controllers/productos.controllers.js";
+
+const router = Router();
+
+router.route("/productos").get(listaProductos);
+
+export default router;
