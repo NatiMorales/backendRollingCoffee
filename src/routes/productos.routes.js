d89544581d9413eeef06b5c6eed346661aa2dd96
Natalia Morales <natii.vmorales98@gmail.com>
2024-03-21 20:56:02 -0300

    agregamos la funcion para eliminar un producto

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { crearProducto, editarProducto, listaProductos, obtenerProducto } from "../controllers/productos.controllers.js";
+import { borrarProducto, crearProducto, editarProducto, listaProductos, obtenerProducto } from "../controllers/productos.controllers.js";
 
 const router = Router();
 
 router.route("/productos").get(listaProductos).post(crearProducto);
-router.route("/producto/:id").get(obtenerProducto).put(editarProducto);
+router.route("/producto/:id").get(obtenerProducto).put(editarProducto).delete(borrarProducto);
 
 export default router;
