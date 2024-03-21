44f4570cf18720887207c73f3b89ccf22b1a5c86
Natalia Morales <natii.vmorales98@gmail.com>
2024-03-21 18:28:57 -0300

    agregamos la logica correspondiente para obtener un array de productos

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,8 +1,17 @@
 import Producto from "../database/model/producto.js";
 
-export const listaProductos = (req, res) => {
-  console.log("desde listar productos");
-  res.send("Enviar lista de productos...");
+export const listaProductos = async(req, res) => {
+  try {
+    //pedir a la BD la lista de la collections de productos
+    const productos = await Producto.find();
+    res.status(200).json(productos);
+    
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+        mensaje:"No se pudo encontrar la lista de productos"
+    })
+  }
 
 };
 
