fdac951038079b96ea4a2306dce473df3ce6f962
Natalia Morales <natii.vmorales98@gmail.com>
2024-04-10 17:59:56 -0300

    agregamos las validaciones de imagen, categoria, descripcion breve y descripcion amplia

--- src/helpers/validacionProducto.js
+++ src/helpers/validacionProducto.js
@@ -19,6 +19,30 @@ const validacionesProducto = [
         throw new Error("El precio debe estar entre $50 y $1000");
       }
     }),
+  check("imagen")
+    .notEmpty()
+    .withMessage("La imagen es un dato obligatorio")
+    .matches(/(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png)/)
+    .withMessage(
+      "La imagen debe tener un formato de URL valida y terminar en jpg|jpeg|gif|png"
+    ),
+  check("categoria")
+    .notEmpty()
+    .withMessage("La categoria es un dato obligatorio")
+    .isIn(["Infusiones", "Batidos", "Dulce", "Salado"])
+    .withMessage(
+      "La categoria debe ser una de las siguientes opciones: 'Infusiones', 'Batidos', 'Dulce', 'Salado'"
+    ),
+  check("descripcionBreve")
+    .notEmpty()
+    .withMessage("La descripcion breve es un dato obligatorio")
+    .isLength({ min: 5, max: 50 })
+    .withMessage("La descripcion debe tener entre 5 y 50 caracteres"),
+  check("descripcionAmplia")
+    .notEmpty()
+    .withMessage("La descripcion amplia es un dato obligatorio")
+    .isLength({ min: 50, max: 300 })
+    .withMessage("La descripcion amplia debe tener entre 50 y 300 caracteres"),
   (req, res, next) => {
     resultadoValidacion(req, res, next);
   },
