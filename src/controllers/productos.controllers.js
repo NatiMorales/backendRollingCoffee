27093faf75bde55b71461a22137e80fea2c87b7c
Natalia Morales <natii.vmorales98@gmail.com>
2024-04-10 17:07:15 -0300

    separamos el resultado de la validacion y los check de express-validator dentro de la carpeta helpers

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -31,11 +31,7 @@ export const obtenerProducto = async (req, res) => {
 export const crearProducto = async (req, res) => {
   try {
     //verificar los datos del body
-    const errors = validationResult(req);
-    //preguntar si ocurrieron erroreres
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errores: errors.array() });
-    }
+    
     //validar los datos
     //pedir a la BD crear el producto
     const productoNuevo = new Producto(req.body);
---
+++ src/helpers/validacionProducto.js
@@ -0,0 +1,27 @@
+import { check } from "express-validator";
+import resultadoValidacion from "./resultadoValidacion.js";
+
+const validacionesProducto = [
+  check("nombreProducto")
+    .notEmpty()
+    .withMessage("El nombre del producto es un dato obligatorio")
+    .isLength({ min: 2, max: 50 })
+    .withMessage("El nombre del producto debe tener entre 2 y 50 caracteres"),
+  check("precio")
+    .notEmpty()
+    .withMessage("El precio del producto es un dato obligatorio")
+    .isNumeric()
+    .withMessage("El precio debe ser un número")
+    .custom((value) => {
+      if (value >= 50 && value <= 10000) {
+        return true;
+      } else {
+        throw new Error("El precio debe estar entre $50 y $1000");
+      }
+    }),
+  (req, res, next) => {
+    resultadoValidacion(req, res, next);
+  },
+];
+
+export default validacionesProducto;
