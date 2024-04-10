f311e87c483142d3b41817b83fd36d5a5fae9dd2
Natalia Morales <natii.vmorales98@gmail.com>
2024-04-10 16:27:51 -0300

    agregamos validaciones del nombreProducto y precio en producto.routes

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,38 +1,41 @@
+import { validationResult } from "express-validator";
 import Producto from "../database/model/producto.js";
 
-export const listaProductos = async(req, res) => {
+export const listaProductos = async (req, res) => {
   try {
     //pedir a la BD la lista de la collections de productos
     const productos = await Producto.find();
     res.status(200).json(productos);
-
   } catch (error) {
     console.log(error);
     res.status(404).json({
-        mensaje:"No se pudo encontrar la lista de productos"
-    })
+      mensaje: "No se pudo encontrar la lista de productos",
+    });
   }
-
 };
 
-export const obtenerProducto = async(req, res) => {
-    try {
-        console.log(req.params.id)
-        //si encontre el producto
-        const productoBuscado = await Producto.findById(req.params.id);
-        res.status(200).json(productoBuscado);
-    } catch (error) {
-        console.log(error);
-        res.status(404).json({
-            mensaje: "No se encontro el producto solicitado"
-        })
-    }
-}
+export const obtenerProducto = async (req, res) => {
+  try {
+    console.log(req.params.id);
+    //si encontre el producto
+    const productoBuscado = await Producto.findById(req.params.id);
+    res.status(200).json(productoBuscado);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje: "No se encontro el producto solicitado",
+    });
+  }
+};
 
 export const crearProducto = async (req, res) => {
- try {
+  try {
     //verificar los datos del body
-    console.log(req.body)
+    const errors = validationResult(req);
+    //preguntar si ocurrieron erroreres
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errores: errors.array() });
+    }
     //validar los datos
     //pedir a la BD crear el producto
     const productoNuevo = new Producto(req.body);
@@ -40,59 +43,62 @@ export const crearProducto = async (req, res) => {
 
     //enviar el status 201
     res.status(201).json({
-        mensaje:"El producto fue creado correctamente"
-    })
-    
- }catch (error) {
+      mensaje: "El producto fue creado correctamente",
+    });
+  } catch (error) {
     console.log(error);
     res.status(400).json({
-        mensaje: "No se pudo procesar la solicitud de crear producto"
-    })
- }
-}
+      mensaje: "No se pudo procesar la solicitud de crear producto",
+    });
+  }
+};
 
-export const editarProducto = async(req, res) => {
-    try {
-        //extraer el id y buscar el producto
-        //si no encuentro el id responder con un mensaje de error
-        const buscarProducto = await Producto.findById(req.params.id)
-        //si no encontre el producto
-        if(!buscarProducto){
-            return res.status(404).json({
-                mensaje:"No se pudo editar el producto, el id es incorrecto."
-            })
-        }
-        //pedir a la BD modificar el producto que tiene tal id, con los datos req.body
-        await Producto.findByIdAndUpdate(req.params.id, req.body)
-        //contestar con mensaje que todo salio bien
-        res.status(200).json({
-            mensaje:"El producto fue modificado exitosamente"
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({mensaje: "Ocurrio un error al intentar editar el producto"})
+export const editarProducto = async (req, res) => {
+  try {
+    //extraer el id y buscar el producto
+    //si no encuentro el id responder con un mensaje de error
+    const buscarProducto = await Producto.findById(req.params.id);
+    //si no encontre el producto
+    if (!buscarProducto) {
+      return res.status(404).json({
+        mensaje: "No se pudo editar el producto, el id es incorrecto.",
+      });
     }
-}
+    //pedir a la BD modificar el producto que tiene tal id, con los datos req.body
+    await Producto.findByIdAndUpdate(req.params.id, req.body);
+    //contestar con mensaje que todo salio bien
+    res.status(200).json({
+      mensaje: "El producto fue modificado exitosamente",
+    });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ mensaje: "Ocurrio un error al intentar editar el producto" });
+  }
+};
 
-export const borrarProducto = async(req, res) => {
-    try {
-        //extraer el id y buscar el producto
-        //si no encuentro el id responder con un mensaje de error
-        const buscarProducto = await Producto.findById(req.params.id)
-        //si no encontre el producto
-        if(!buscarProducto){
-            return res.status(404).json({
-                mensaje:"No se pudo eliminar el producto, el id es incorrecto."
-            })
-        }
-        //pedir a la BD modificar el producto que tiene tal id, con los datos req.body
-        await Producto.findByIdAndDelete(req.params.id)
-        //contestar con mensaje que todo salio bien
-        res.status(200).json({
-            mensaje:"El producto fue eliminado exitosamente"
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({mensaje: "Ocurrio un error al intentar borrar el producto"})
+export const borrarProducto = async (req, res) => {
+  try {
+    //extraer el id y buscar el producto
+    //si no encuentro el id responder con un mensaje de error
+    const buscarProducto = await Producto.findById(req.params.id);
+    //si no encontre el producto
+    if (!buscarProducto) {
+      return res.status(404).json({
+        mensaje: "No se pudo eliminar el producto, el id es incorrecto.",
+      });
     }
-}
+    //pedir a la BD modificar el producto que tiene tal id, con los datos req.body
+    await Producto.findByIdAndDelete(req.params.id);
+    //contestar con mensaje que todo salio bien
+    res.status(200).json({
+      mensaje: "El producto fue eliminado exitosamente",
+    });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ mensaje: "Ocurrio un error al intentar borrar el producto" });
+  }
+};
